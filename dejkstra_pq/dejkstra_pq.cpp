2534afdb43de225bf015ba741da0cf11d14b36f7
SN4KEBYTE <timurcat7@gmail.com>
2019-05-23 19:58:40 +0700

    Cosmetic changes.

--- dejkstra_pq/dejkstra_pq.cpp
+++ dejkstra_pq/dejkstra_pq.cpp
@@ -5,34 +5,37 @@
 #include <chrono>
 #include <string>
 
+typedef unsigned int UINT;
+typedef unsigned __int64 UINT64;
+
 using namespace std;
-// ctrl + f5
-const int TESTS_NUMBER = 1;
-const unsigned __int64 INF = numeric_limits<unsigned __int64>::max();
 
-vector<vector<int>> scan(ifstream &in, const int &n, const int &m);
+const UINT TESTS_NUMBER = 1;
+const UINT64 INF = numeric_limits<UINT64>::max();
 
-vector<unsigned __int64> dejkstra_pq(const vector<vector<int>> graph, const int &n, const int &start);
+vector<vector<UINT>> scan(ifstream &in, const UINT &n, const UINT &m);
+
+vector<UINT64> dejkstra_pq(const vector<vector<UINT>> graph, const UINT &n, const UINT &start);
 
 int main()
 {
-   for (int cnt = 0; cnt < TESTS_NUMBER; cnt++)
+   for (UINT cnt = 0; cnt < TESTS_NUMBER; cnt++)
    {
       ifstream in("tests/graph" + to_string(cnt + 1) + ".txt");
 
-      int n = 0, m = 0;
+      UINT n = 0, m = 0;
       in >> n >> m;
 
-      vector<vector<int>> graph = scan(in, n, m);
+      vector<vector<UINT>> graph = scan(in, n, m);
 
-      int start = 0;
+      UINT start = 0;
       in >> start;
 
       in.close();
 
       auto t_start = chrono::high_resolution_clock().now();
 
-      vector<unsigned __int64> res = dejkstra_pq(graph, n, start);
+      vector<UINT64> res = dejkstra_pq(graph, n, start);
 
       auto t_end = chrono::high_resolution_clock().now() - t_start;
       double elapsed_time = chrono::duration<double>(t_end).count();
@@ -41,7 +44,7 @@ int main()
 
       out << "N = " << n << ", M = " << m << endl << "Elapsed time: " << fixed << elapsed_time << " seconds" << endl;
 
-      for (int i = 0; i < res.size(); i++)
+      for (UINT i = 0; i < res.size(); i++)
       {
          out << start << "-" << i + 1 << " = ";
 
@@ -57,13 +60,13 @@ int main()
    return 0;
 }
 
-vector<vector<int>> scan(ifstream &in, const int &n, const int &m)
+vector<vector<UINT>> scan(ifstream &in, const UINT &n, const UINT &m)
 {
-   vector<vector<int>> graph(n, vector<int>(n));
+   vector<vector<UINT>> graph(n, vector<UINT>(n));
 
-   for (int i = 0; i < m; i++)
+   for (UINT i = 0; i < m; i++)
    {
-      int u = 0, v = 0, w = 0;
+      UINT u = 0, v = 0, w = 0;
       in >> u >> v >> w;
 
       graph[u - 1][v - 1] = graph[v - 1][u - 1] = w;
@@ -72,20 +75,20 @@ vector<vector<int>> scan(ifstream &in, const int &n, const int &m)
    return graph;
 }
 
-vector<unsigned __int64> dejkstra_pq(const vector<vector<int>> graph, const int &n, const int &start)
+vector<UINT64> dejkstra_pq(const vector<vector<UINT>> graph, const UINT &n, const UINT &start)
 {
-   priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-   vector<unsigned __int64> res(n, INF);
+   priority_queue<pair<UINT, UINT>, vector<pair<UINT, UINT>>, greater<pair<UINT, UINT>>> pq;
+   vector<UINT64> res(n, INF);
 
    pq.push(make_pair(0, start - 1));
    res[start - 1] = 0;
 
    while (!pq.empty())
    {
-      pair<int, int> s = pq.top();
+      pair<UINT, UINT> s = pq.top();
       pq.pop();
 
-      for (int i = 0; i < n; i++)
+      for (UINT i = 0; i < n; i++)
          if (graph[s.second][i] && res[i] > res[s.second] + graph[s.second][i])
          {
             res[i] = res[s.second] + graph[s.second][i];
